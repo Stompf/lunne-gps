0c8edf1ccdf07e25e2bb24b3af12e14536e2a3a3
Stompf <anton.lundmark@gmail.com>
2021-01-04 18:07:22 +0100

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    env: {
+        browser: true,
+        node: true,
+    },
+    extends: ['airbnb-typescript', 'prettier', 'prettier/@typescript-eslint', 'prettier/react'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: 'tsconfig.json',
+        sourceType: 'module',
+    },
+    plugins: ['prettier'],
+    rules: {
+        'import/prefer-default-export': ['off'],
+        '@typescript-eslint/no-unused-vars': ['off'],
+        'import/no-unresolved': ['off'],
+        '@typescript-eslint/no-use-before-define': ['off'],
+        'eol-last': ['error', 'always'],
+        'prettier/prettier': [
+            'error',
+            {
+                endOfLine: 'auto',
+            },
+        ],
+    },
+};
