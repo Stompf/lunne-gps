7c27c74d1d48bcf8654317a00ab763524d8a9a36
Stompf <anton.lundmark@gmail.com>
2021-01-16 12:33:21 +0100

    material ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
-import '../styles/globals.css';
-
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import type { AppProps /* , AppContext */ } from 'next/app';
+import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    return <Component {...pageProps} />;
+    React.useEffect(() => {
+        // Remove the server-side injected CSS.
+        const jssStyles = document.querySelector('#jss-server-side');
+        if (jssStyles) {
+            jssStyles.parentElement?.removeChild(jssStyles);
+        }
+    }, []);
+
+    return (
+        <>
+            <Head>
+                <title>My page</title>
+                <meta
+                    name="viewport"
+                    content="minimum-scale=1, initial-scale=1, width=device-width"
+                />
+            </Head>
+            <ThemeProvider theme={theme}>
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline />
+                {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+    );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
+import UploadMap from './upload-map';
 
 export default function Home() {
     return (
         <main>
             <div id="map">
                 <MapWithNoSSR />
+                <UploadMap />
             </div>
         </main>
     );
