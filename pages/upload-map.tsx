582868305d96aff1e22d52d5f088884e070385ee
Stompf <anton.lundmark@gmail.com>
2021-01-26 23:00:14 +0100

    add upload stuff

--- pages/upload-map.tsx
+++ pages/upload-map.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import { Input, Typography } from '@material-ui/core';
 import { ResponseUploadGpx } from '../common/upload-gpx.response';
 
 interface UploadMapProps {
@@ -10,57 +10,59 @@ interface UploadMapProps {
     currentMap: ResponseUploadGpx | null;
 }
 
-const UploadMap: React.FC<UploadMapProps> = (props) => (
-    <Container maxWidth="sm">
-        <Box my={4}>
-            {/* <Typography variant="h4" component="h1" gutterBottom>
-                    Next.js example
-                </Typography> */}
-            <Grid container spacing={5} alignItems="flex-end">
+const UploadMap: React.FC<UploadMapProps> = (props) => {
+    const [fileName, setFileName] = useState<string>('');
+
+    const handleOnChange = async (event: React.FormEvent<HTMLInputElement>) => {
+        const { files } = event.target as HTMLInputElement;
+
+        if (!files) {
+            return;
+        }
+
+        const response = await fetch('/api/upload-gpx', {
+            method: 'POST',
+            body: files[0],
+            headers: {
+                // Content-Type may need to be completely **omitted**
+                // or you may need something
+                'Content-Type': files[0].type,
+            },
+        });
+
+        const result: ResponseUploadGpx = await response.json();
+
+        setFileName(files[0].name);
+        props.setMap(result);
+    };
+
+    return (
+        <Container maxWidth="sm">
+            <Box my={4}>
                 <Button variant="contained" component="label" color="primary">
                     Upload File
-                    <input
-                        accept=".gpx"
-                        type="file"
-                        hidden
-                        onChangeCapture={(e) => handleOnChange(e, props)}
-                    />
+                    <input accept=".gpx" type="file" hidden onChangeCapture={handleOnChange} />
                 </Button>
-                <Button
-                    variant="contained"
-                    component="label"
-                    color="secondary"
-                    onClick={onSaveMap}
-                    disabled={props.currentMap == null}
-                >
-                    Save
-                </Button>
-            </Grid>
-        </Box>
-    </Container>
-);
+                <Box my={4}>
+                    <Typography variant="body1" component="p" gutterBottom>
+                        {fileName}
+                    </Typography>
+                </Box>
+                <Box my={4} hidden={props.currentMap == null}>
+                    <Input placeholder="Prefix (optional)" />
+                    <Button
+                        variant="contained"
+                        component="label"
+                        color="secondary"
+                        onClick={onSaveMap}
+                    >
+                        Save
+                    </Button>
+                </Box>
+            </Box>
+        </Container>
+    );
+};
 export default UploadMap;
-
-async function handleOnChange(event: React.FormEvent<HTMLInputElement>, props: UploadMapProps) {
-    const { files } = event.target as HTMLInputElement;
-
-    if (!files) {
-        return;
-    }
-
-    const response = await fetch('/api/upload-gpx', {
-        method: 'POST',
-        body: files[0],
-        headers: {
-            // Content-Type may need to be completely **omitted**
-            // or you may need something
-            'Content-Type': files[0].type,
-        },
-    });
-
-    const result: ResponseUploadGpx = await response.json();
-
-    props.setMap(result);
-}
 
 function onSaveMap() {}
